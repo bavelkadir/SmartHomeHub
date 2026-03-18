20f0d1e7a6f7fe1edd8b829ef765c132837ec4b7
bavelkadir <bavel.kadiir@gmail.com>
2026-03-18 20:50:04 +0100

    Jag har skapat Devices -Lamp -Thermostats -Doorlock -IDevice

---
+++ Devices/Lamp.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SmartHomeHub.Devices;
+
+namespace SmartHomeHub.Devices
+{
+    public class Lamp : IDevice
+    {
+        public string Name { get; private set; }
+        
+        private bool isOn;
+
+        public Lamp(string name)
+        {
+            Name = name;
+        }
+
+        public void TurnOn()
+        {
+            isOn = true;
+            Console.WriteLine($"{Name} is ON");
+        }
+
+        public void TurnOff()
+        {
+            isOn = false;
+            Console.WriteLine($"{Name} is OFF");
+        }
+    }
+}
---
+++ Devices/Thermostat.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SmartHomeHub.Devices
+{
+    public class Thermostat : IDevice
+    {
+
+        public string Name { get; }
+        private double temperature;
+
+        private bool isOn;
+
+        public Thermostat(string name)
+        {
+            Name = name;
+        }
+
+        public void TurnOn()
+        {
+            isOn = true;
+            Console.WriteLine($"{Name} is on");
+        }
+
+        public void TurnOff()
+        {
+            isOn = false;
+            Console.WriteLine($"{Name} is off");
+        }
+
+        public void SetTemperature(double temp)
+        {
+            temperature = temp;
+            Console.WriteLine($"{Name} set to {temperature}°C");
+        }
+
+
+    }
+}
