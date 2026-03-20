0d583d8e1db3acdd2e24bcd4fd49da8a7e1ad9ce
bavelkadir <bavel.kadiir@gmail.com>
2026-03-20 17:14:34 +0100

    Fixade fcade la allt som en klass som sedan program.cs kan bara anropa ifrån, alltså jag har samlatt allt i ett ställe så att inte main blir som en spagethi

--- Strategy/PartyModeStrategy.cs
+++ Strategy/PartyModeStrategy.cs
@@ -1,17 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-
 using System;
+
 
 namespace SmartHomeHub.Strategy
 {    
+
     // koden nedan visar en strategi för "PartyMode", där alla åtgärder är tillåtna 
     public class PartyModeStrategy : IModeStrategy
     {
         public void ApplyMode(string deviceName, string action)
         {
-            Console.WriteLine($"[PartyMode] EVERYTHING ALLOWED 🎉 {deviceName}: {action}");
+            Console.WriteLine($"[PartyMode] EVERYTHING ALLOWED {deviceName}: {action}");
         }
     }
 }
